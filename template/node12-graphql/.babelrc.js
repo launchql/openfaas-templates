5e9afe2b397f44767e18bbec25eac2f60986a63c
Dan Lynch <pyramation@gmail.com>
2020-12-30 01:22:21 -0700

    nullish coalescing operator and optional chaining

--- template/node12-graphql/.babelrc.js
+++ template/node12-graphql/.babelrc.js
@@ -5,6 +5,8 @@ module.exports = (api) => {
   return {
     plugins: [
       ['@babel/transform-runtime', { useESModules }],
+      "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-proposal-nullish-coalescing-operator",
       '@babel/proposal-object-rest-spread',
       '@babel/proposal-class-properties',
       '@babel/proposal-export-default-from',
